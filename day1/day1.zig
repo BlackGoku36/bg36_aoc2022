1e61a5f68d66c1016935de98b4f00bc0ad632ba4
BlackGoku36 <urjasvisuthar@gmail.com>
2022-12-01 16:20:06 +0530

    make day1 less stupid

--- day1/day1.zig
+++ day1/day1.zig
@@ -1,21 +1,10 @@
 const std = @import("std");
 
-fn part1() anyerror!void {
-    var file = try std.fs.cwd().openFile("input.txt", .{});
-    defer file.close();
-
-    var buffer: [2000]u8 = undefined;
-
+fn part1(all_calories: []u32) anyerror!void {
     var calories: u32 = 0;
-    var temp_cal: u32 = 0;
 
-    while (try file.reader().readUntilDelimiterOrEof(&buffer, '\n')) |line| {
-        var value = std.fmt.parseInt(u32, line, 10) catch {
-            if (temp_cal > calories) calories = temp_cal;
-            temp_cal = 0;
-            continue;
-        };
-        temp_cal += value;
+    for (all_calories) |value| {
+        if (value > calories) calories = value;
     }
 
     std.log.info("Part-1; Calories: {d}", .{calories});
@@ -25,28 +14,10 @@ fn cmpByValue(context: void, a: u32, b: u32) bool {
     return std.sort.desc(u32)(context, a, b);
 }
 
-fn part2() anyerror!void {
-    var file = try std.fs.cwd().openFile("input.txt", .{});
-    defer file.close();
-
-    var buffer: [2000]u8 = undefined;
-
+fn part2(temp_all_calories: []u32) anyerror!void {
     var all_calories: [500]u32 = undefined;
-    for (all_calories) |_, index| {
-        all_calories[index] = 0;
-    }
-
-    var temp_cal: u32 = 0;
-    var i: u32 = 0;
-
-    while (try file.reader().readUntilDelimiterOrEof(&buffer, '\n')) |line| {
-        var value = std.fmt.parseInt(u32, line, 10) catch {
-            all_calories[i] = temp_cal;
-            temp_cal = 0;
-            i += 1;
-            continue;
-        };
-        temp_cal += value;
+    for (temp_all_calories) |_, index| {
+        all_calories[index] = temp_all_calories[index];
     }
 
     std.sort.sort(u32, all_calories[0..], {}, cmpByValue);
@@ -55,6 +26,28 @@ fn part2() anyerror!void {
 }
 
 pub fn main() anyerror!void {
-    try part1();
-    try part2();
+    var all_calories: [500]u32 = undefined;
+    for (all_calories) |_, index| {
+        all_calories[index] = 0;
+    }
+
+    var file = try std.fs.cwd().openFile("input.txt", .{});
+    defer file.close();
+
+    var calorie: u32 = 0;
+    var buffer: [2000]u8 = undefined;
+
+    var i: u32 = 0;
+    while (try file.reader().readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        var value = std.fmt.parseInt(u32, line, 10) catch {
+            all_calories[i] = calorie;
+            calorie = 0;
+            i += 1;
+            continue;
+        };
+        calorie += value;
+    }
+
+    try part1(&all_calories);
+    try part2(&all_calories);
 }
